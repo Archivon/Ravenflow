68e5c9ef7c04ee22a3eda304189f90f2ba410cd6
juliareboucasleite <69313019+juliareboucasleite@users.noreply.github.com>
2026-01-19 15:40:18 +0000

    Create database.config.js

---
+++ config/database.config.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'ravenflow',
+    port: process.env.DB_PORT || 3306
+  },
+  production: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
+  }
+};
